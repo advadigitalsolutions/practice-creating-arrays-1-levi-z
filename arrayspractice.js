a17c9471145b042f6e76a0b1c01690b4ebcabb5c
Lee Zuvanich <leezuvanich@macbookair.lan>
2025-03-22 23:25:05 -0500

    updated file with commited code

---
+++ arrayspractice.js
@@ -0,0 +1,13 @@
+let groceryItems = ["Bread", "Milk", "Eggs", "Cheese", "Apples"];
+let prices =[2.99, 3.99, 1.99, 4.99, 2.99];
+
+console.log( "Grocery Items:", groceryItems);
+
+console.log("Prices:", prices);
+
+console.log("First item:", groceryItems[0]);
+console.log("Second item:", groceryItems[3]);
+console.log("Third item:", groceryItems[4]);
+
+console.log("First price:", prices[0]);
+console.log("Second price:", prices[3]);
